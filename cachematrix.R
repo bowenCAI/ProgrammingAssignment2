01c19531a84ee1811dc83ff063c0847b36875d61
bowenCAI <tom.caibowen@gmail.com>
2016-01-21 11:03:58 +0800

    Complete the assignment 2
    I have completed this assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseM<-NULL
+    #set the value of the matrix
+    set<-function(y){
+        x<<-y
+        inverseM<<-NULL
+    }
+    #get the value of the matrix
+    get<-function() x
+    #set the value of inverse of the matrix
+    setinverse<-function(inverse) inverseM<<-inverse
+    #get the value of inverse of the matrix
+    getinverse<-function() inverseM
+    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverseM<-x$getinverse()
+    if(!is.null(inverseM)){
+        message("getting cached data")
+        return(inverseM)
+    }
+    data<-x$get()
+    inverseM<-solve(data,...)
+    x$setinverse(inverseM)
+    inverseM
 }
+
+#testing
+x<- rbind(c(1,2),c(2,1))
+m<-makeCacheMatrix(x)
+m$get()#display the matrix
+m$getinverse()#get the inverse matrix
+cacheSolve(m)#display the cached data
